bd57c8c786b0bab17e381430f3543422e1b4f135
Anthony4834 <77992188+Anthony4834@users.noreply.github.com>
2021-04-06 13:50:27 -0700

    inital commit

---
+++ py_theor/Pythag.java
@@ -0,0 +1,6 @@
+public class Pythag {
+    public double findHyp(int a, int b) {
+        double result = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        return result;
+    }
+}
---
+++ py_theor/PythagTest.java
@@ -0,0 +1,6 @@
+public class PythagTest {
+    public static void main(String[] args) {
+        Pythag Pythagorean = new Pythag();
+        System.out.println(Pythagorean.findHyp(3, 5));
+    }
+}
