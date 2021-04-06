182ac583b4eee8557f95d1239e02cea829dc7f8c
Anthony4834 <77992188+Anthony4834@users.noreply.github.com>
2021-04-06 14:24:14 -0700

    Added FizzBuzz

--- pythagorean_theorem/Pythag.java
+++
@@ -1,6 +0,0 @@
-public class Pythag {
-    public double findHyp(int a, int b) {
-        double c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-        return c;
-    }
-}
--- pythagorean_theorem/PythagTest.java
+++
@@ -1,6 +0,0 @@
-public class PythagTest {
-    public static void main(String[] args) {
-        Pythag Pythagorean = new Pythag();
-        System.out.println(Pythagorean.findHyp(3, 5));
-    }
-}
