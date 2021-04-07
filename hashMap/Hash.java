36819a0ef73cf2b03998c22c1f72c06b15306d81
Anthony4834 <77992188+Anthony4834@users.noreply.github.com>
2021-04-06 21:46:52 -0700

    Added hashMap

---
+++ hashMap/Hash.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class Hash {
+    HashMap<String, String> map = new HashMap<String, String>();
+
+    public Hash() {
+        map.put("Song 1", "Lyrics 1");
+        map.put("Song 2", "Lyrics 2");
+        map.put("Song 3", "Lyrics 3");
+        map.put("Song 4", "Lyrics 4");
+    }
+}
---
+++ hashMap/Run.java
@@ -0,0 +1,14 @@
+import java.util.Set;
+
+public class Run {
+    public static void main(String[] args) {
+            Hash hash = new Hash();
+            
+            System.out.println(hash.map.get("Song 3")); //print one
+
+            Set<String> mapKeys = hash.map.keySet(); //print all
+            for(String key : mapKeys) {
+                System.out.println(hash.map.get(key));
+            } 
+    }
+}
